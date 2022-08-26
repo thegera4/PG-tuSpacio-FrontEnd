d4a65a9ca96d2e69f626f55e4197d206b5f14aba
thegera4 <84020433+thegera4@users.noreply.github.com>
2022-08-25 22:56:33 -0500

    Estilos cambiados

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { alpha, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -15,74 +14,9 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import LogoIMG from '../../assets/images/img_logo.jpeg';
 import LogoFONT from '../../assets/images/font_logo.jpeg';
-import './Navbar.css';
-
-const useStyles = makeStyles((theme) => ({
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    //marginRight: 0,
-    //marginLeft: theme.spacing(2),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    marginLeft: 'auto',
-    [theme.breakpoints.up('sm')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    marginLeft: 'auto',
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
-}));
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../../ThemeConfig';
+import useStyles from './useStyles';
 
 export default function Navbar() {
   const classes = useStyles();
@@ -113,68 +47,75 @@ export default function Navbar() {
   );
 
   return (
-    <div className={classes.grow}>
-      <AppBar position="static" style={{ background: '#257558' }} >
-        <Toolbar>
-          {/* Hamburguesa */}
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-          {/* Logo */}
-          <Box>
-            <img className="logo" src={LogoIMG} alt="logo img"/>
-            <img className="logo-font" src={LogoFONT} alt="logo font"/>
-          </Box>
-          {/* Searchbar */}
-          <div className={classes.search} /*style={{ marginLeft: '10em'}}*/>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
+    <ThemeProvider theme={theme}>
+      <div className={classes.grow}>
+        <AppBar position="static" >
+          <Toolbar>
+            {/* Hamburguesa */}
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer">
+                <MenuIcon />
+            </IconButton>
+            {/* Logo */}
+            <Box className={classes.logoBox}>
+              <img 
+                className={classes.logoImg} 
+                src={LogoIMG} 
+                alt="logo img"/>
+              <img 
+                className={classes.logoFont} 
+                src={LogoFONT} 
+                alt="logo font"/>
+            </Box>
+            {/* Searchbar */}
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
             </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
-          {/* Iconos de carrito y perfil */}
-          <div className={classes.sectionDesktop}>
-            <IconButton color="inherit" >
-              <Badge color="secondary">
-                <ShoppingCartIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </div>
-          {/* Icono de tres puntos paraa mobile */}
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label="show more"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      {renderMenu}
-    </div>
+            {/* Iconos de carrito y perfil */}
+            <div className={classes.sectionDesktop}>
+              <IconButton color="inherit" >
+                <Badge color="secondary">
+                  <ShoppingCartIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
+            {/* Icono de tres puntos para mobile */}
+            <div className={classes.sectionMobile}>
+              <IconButton
+                aria-label="show more"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        {renderMenu}
+      </div>
+    </ThemeProvider>
   );
 }
