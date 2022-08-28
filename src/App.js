ea39c4c7fed9eb9f65961e80bbfe7fe83c892db0
Adrian Quintana <radrianquintana@gmail.com>
2022-08-28 20:39:25 -0300

    corregi App y Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Footer from './Components/Footer/Footer.jsx';
+import './App.css';
+import {Container} from '@material-ui/core'
 import Navbar from './Components/Navbar/Navbar.jsx';
+import Footer from './Components/Footer/Footer.jsx';
+import SearchBar from './Components/SearchBar/SearchBar';
+import Detail from './Components/Detail/Detail';
+// import ContainerCards from './Components/ContainerCards/ContainerCards.jsx'
 
 function App() {
   return (
-    <div>
+    <Container maxWidth='xl' disableGutters='true' style={{backgroundColor:'#c8e6c9'}} >
+      
       <Navbar />
+      <SearchBar />
       <Footer />
-    </div>
+      <Detail />
+
+      </Container>
+   
   );
 }
 
