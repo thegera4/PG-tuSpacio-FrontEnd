59aaefbde5935f4b5ddbb8a466dafa4f4264b2b2
Diego Maceda <99127823+dmaceda@users.noreply.github.com>
2022-08-31 19:11:32 -0300

    mejorando estilos card y paginado

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,8 @@ import HomeCards from '../../Components/HomeCards/HomeCards';
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllProducts } from '../../actions';
 import BasicPagination from '../BasicPagination/Pagination';
-
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -43,13 +44,12 @@ export default function SpacingGrid() {
   // console.log(products)
   return (
     <>
-      <BasicPagination 
-        className={classes.centering} 
-        currentPage={currentPage}
-        productsPerPage={productsPerPage}
-        totalProducts={products.length}
-        onChange={handleChange}
-        />
+        <Typography component="div">
+      
+      <Box textAlign="left" marginLeft="60px" marginBottom="20px" fontWeight="fontWeightBold" fontSize={20}>
+        All Products 
+      </Box>
+    </Typography>
       <Grid container className={classes.root} spacing={2}>
         <Grid item xs={12}>
           <Grid 
@@ -66,6 +66,13 @@ export default function SpacingGrid() {
           </Grid>
         </Grid>
       </Grid>
+      <BasicPagination 
+        className={classes.centering} 
+        currentPage={currentPage}
+        productsPerPage={productsPerPage}
+        totalProducts={products.length}
+        onChange={handleChange}
+        />
     </>
   );
 }
--- src/Components/HomeCards/useStyles.js
+++ src/Components/HomeCards/useStyles.js
@@ -3,11 +3,18 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
   root: {
     width: 345,
-    height: 450,
+    height: 435,
+    border: '1px solid #eceeed',
+
   },
   media: {
     height: 300,
   },
+  content: {
+    marginTop: 10,
+    height: 100,
+    backgroundColor: "#eceeed"
+  }  
 });
 
 export default useStyles;
