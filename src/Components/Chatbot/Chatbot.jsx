096e9061a18e4c350e0eb4702712aa18e605ceca
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-09-12 00:10:28 -0500

    ADD-STYLES-CHATBOX

--- src/Components/Chatbot/Chatbot.jsx
+++ src/Components/Chatbot/Chatbot.jsx
@@ -40,7 +40,7 @@ export default function Contenido() {
           aria-labelledby="exampleModalLabel"
           aria-hidden="true"
         >
-          <div className="modal-dialog allContainerChat">
+          <div className="modal-dialog allContainerChat mx-auto d-block">
             <div className="modal-content">
               <button
                 type="button"
